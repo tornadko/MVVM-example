20daefa22749a69d07529a6d12c1c8a872ba5a59
Alex Kucherenko <alexiko86@gmail.com>
2016-04-28 22:44:17 +0200

    Added group object and simple binding

--- MVVM_Example/app/src/main/java/example/mvvm/alex/mvvm_example/MainActivity.java
+++ MVVM_Example/app/src/main/java/example/mvvm/alex/mvvm_example/MainActivity.java
@@ -1,13 +1,19 @@
 package example.mvvm.alex.mvvm_example;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import example.mvvm.alex.mvvm_example.databinding.ActivityMainBinding;
+import example.mvvm.alex.mvvm_example.model.Group;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        Group group = new Group("Test group");
+        binding.setGroup(group);
     }
 }
---
+++ MVVM_Example/app/src/main/java/example/mvvm/alex/mvvm_example/model/Group.java
@@ -0,0 +1,12 @@
+package example.mvvm.alex.mvvm_example.model;
+
+/**
+ * Created by Alex on 28.04.2016.
+ */
+public class Group {
+    public final String name;
+
+    public Group(String name) {
+        this.name = name;
+    }
+}
